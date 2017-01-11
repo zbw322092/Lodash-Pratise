9c05318f6857423f86d7e5c8088ce13bef654393
zbw322092 <bz4e14@soton.ac.uk>
2017-01-11 16:26:54 +0800

    objectToString

--- common_functions.js
+++ common_functions.js
@@ -1,3 +1,5 @@
+var objectProto = Object.prototype;
+var nativeObjectToString = objectProto.toString;
 module.exports = {
 	// 这个值被typeof判断为object或者function，且这个值不是null的时候，下列函数返回true，否则为false
 	// 要点：
@@ -13,9 +15,15 @@ module.exports = {
 	// 2. 整数
 	// 3. 大于等于0
 	// 4. 小于等于2^53-1
+	// 
+	// 这里要做类型检查的原因在于，我们如果传入一个空的字符串，下列的检查结果都会是true。
 	isLength: function(value) {
 		return typeof value === "number" && 
 			value % 1 === 0 && value >= 0 && value <= Number.MAX_SAFE_INTEGER;
+	},
+	// 通过调用Object原型中的toString方法来生成一个string，我们可以从这个string中获得类的信息。
+	objectToString: function(value) {
+		return nativeObjectToString.call(value);
 	}
 };
 
@@ -30,14 +38,57 @@ module.exports = {
 // console.log(module.exports.isObject()); // false  在必传入的实参没有传的时候，这个实参被当做undefined处理
 
 
-console.log(module.exports.isLength('')); // false
-console.log(module.exports.isLength(0)); // true
-console.log(module.exports.isLength('0')); // false
-console.log(module.exports.isLength(NaN)); // false
-console.log(module.exports.isLength()); // false
-console.log(module.exports.isLength(undefined)); // false
-console.log(module.exports.isLength(Number.MAX_SAFE_INTEGER)); // true
-console.log(module.exports.isLength(Number.MAX_SAFE_INTEGER + 1)); // false
-console.log(module.exports.isLength(Infinity)); // false
-console.log(module.exports.isLength(-0)); // true
-console.log(module.exports.isLength(1.23)); // false
+// console.log(module.exports.isLength('')); // false
+// console.log(module.exports.isLength(0)); // true
+// console.log(module.exports.isLength('0')); // false
+// console.log(module.exports.isLength(NaN)); // false
+// console.log(module.exports.isLength()); // false
+// console.log(module.exports.isLength(undefined)); // false
+// console.log(module.exports.isLength(Number.MAX_SAFE_INTEGER)); // true
+// console.log(module.exports.isLength(Number.MAX_SAFE_INTEGER + 1)); // false
+// console.log(module.exports.isLength(Infinity)); // false
+// console.log(module.exports.isLength(-0)); // true
+// console.log(module.exports.isLength(1.23)); // false
+
+console.log(module.exports.objectToString({name: 'bowen'})); // [object Object]
+console.log(module.exports.objectToString(123)); // [object Number]
+console.log(module.exports.objectToString('it is a string')); // [object String]
+console.log(module.exports.objectToString(function(){})); // [object Function]
+console.log(module.exports.objectToString(null)); // [object Null]
+console.log(module.exports.objectToString(undefined)); // [object Undefined]
+console.log(module.exports.objectToString(NaN)); // [object Number]
+console.log(module.exports.objectToString([1,2,3])); // [object Array]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
