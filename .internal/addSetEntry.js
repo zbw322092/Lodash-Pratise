24056285d81b37ba1e898a07a85e19f1187721a9
Bowen Zhong <bz4e14@soton.ac.uk>
2017-01-22 14:30:19 +0800

    addSetEntry

---
+++ .internal/addSetEntry.js
@@ -0,0 +1,17 @@
+/**
+ * Adds `value` to `set`.
+ *
+ * @private
+ * @param {Object} set The set to modify.
+ * @param {*} value The value to add.
+ * @returns {Object} Returns `set`.
+ */
+function addSetEntry(set, value) {
+	set.add(value);
+	return set;
+}
+
+console.log(addSetEntry(new Set(), null)); // Set { null }
+console.log(addSetEntry(new Set(), [1,2,3])); // Set { [ 1, 2, 3 ] }
+
+export default addSetEntry;
