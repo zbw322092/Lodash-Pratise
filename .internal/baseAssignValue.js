65edbee465d8f7ceb211b93c434bcf087d5a88ed
zbw322092 <bz4e14@soton.ac.uk>
2017-01-23 23:05:01 +0800

    baseAssignValue

---
+++ .internal/baseAssignValue.js
@@ -0,0 +1,10 @@
+/**
+ * The base implementation of `assignValue` and `assignMergeValue` without
+ * value checks.
+ *
+ * @private
+ * @param {Object} object The object to modify.
+ * @param {string} key The key of the property to assign.
+ * @param {*} value The value to assign.
+ */
+
